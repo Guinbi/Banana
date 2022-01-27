2488e60b3c6eb25dc663fb8eb53a88f91a060f28
Guinbi <90347338+Guinbi@users.noreply.github.com>
2022-01-26 21:48:06 -0300

    Update

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let image = document.querySelector("img");
 image.onclick = function(){
 	let src = image.getAttribute("src");
 	
-	if(src === "imagens/banana.jfif"){
-		image.setAttribute("src", "imagens/therock.jfif");
+	if(src === "banana.jfif"){
+		image.setAttribute("src", "therock.jfif");
 		document.getElementById("eyebrow").play();
 	} else {
-		image.setAttribute("src", "imagens/banana.jfif");
+		image.setAttribute("src", "banana.jfif");
 	}
 }
